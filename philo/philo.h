f760cd087af3b5b49448c11a4dedc74a46712089
Oussama Mimouni <omimouni33@gmail.com>
2024-03-21 03:16:24 +0000

    utils

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: oumimoun <oumimoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 19:33:24 by oumimoun          #+#    #+#             */
-/*   Updated: 2024/03/09 19:34:53 by oumimoun         ###   ########.fr       */
+/*   Updated: 2024/03/20 23:10:14 by oumimoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,32 @@
 
 #define PHILO_H
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <limits.h>
-# include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define MAX_PHILOSOPHERS 10
+
+pthread_mutex_t forks[MAX_PHILOSOPHERS];
 
 typedef struct s_data
 {
-    void *data;
-    // struct s_data *next;
+    int nb_philo;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int nb_must_eat;
+    int times_eaten;
+    pthread_mutex_t *forks;
+    // long start_time;
 
-} t_data;
+} t_philo;
+
+int	ft_atoi(char *str);
+
 
 #endif
--- philo/philo_utils.c
+++ philo/philo_utils.c
@@ -1,19 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   philo_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oumimoun <oumimoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/09 19:28:40 by oumimoun          #+#    #+#             */
-/*   Updated: 2024/03/09 19:40:39 by oumimoun         ###   ########.fr       */
+/*   Created: 2024/03/20 00:53:53 by oumimoun          #+#    #+#             */
+/*   Updated: 2024/03/20 22:50:38 by oumimoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main()
+int	ft_atoi(char *str)
 {
-    printf("hello pp");
-    return (0);
+	int i;
+	int signe;
+	int total;
+
+	i = 0;
+	signe = 1;
+	total = 0;
+	while (str[i] >= ' ' || str[i] == '\t')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			signe *= (-1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		total = total * 10 + str[i] - '0';
+		i++;
+	}
+	return (signe * total);
 }
