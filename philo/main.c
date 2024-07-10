d3e43674cc9b52f48b4dfd4571c55c8369b899a9
Oussama Mimouni <omimouni33@gmail.com>
2024-07-10 17:12:14 +0100

    updated

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: oumimoun <oumimoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 08:50:39 by oumimoun          #+#    #+#             */
-/*   Updated: 2024/07/07 15:19:48 by oumimoun         ###   ########.fr       */
+/*   Updated: 2024/07/09 10:06:18 by oumimoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ int	main(int ac, char **av)
 	t_data	*data;
 
 	data = malloc(sizeof(t_data));
+	if (!data)
+		return (ERROR);
 	if (ft_parse_args(ac, av, data) == ERROR)
 	{
 		ft_putstr_fd("Error: wrong arguments\n", 2);
